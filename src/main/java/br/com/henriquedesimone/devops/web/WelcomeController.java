7484fd82575b08c1e88a58e79fad804485d80bb2
Henrique De Simone <eu@henriquedesimone.com.br>
2020-06-29 18:08:38 -0300

    Assets

---
+++ src/main/java/br/com/henriquedesimone/devops/web/WelcomeController.java
@@ -0,0 +1,15 @@
+package br.com.henriquedesimone.devops.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("course", "Devops");
+        return "index";
+    }
+}
